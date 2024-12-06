df5cc10073c3eaef992aca84d24d0bca45b37c46
Pablozalazararg <101354957+Pablozalazararg@users.noreply.github.com>
2024-12-06 16:36:02 -0300

    Delete src directory

--- src/components/Note.jsx
+++
@@ -1,8 +0,0 @@
-const Note = ({note,number, toggleImportance}) => {
-  
-  return (
-    <div>{note} {number} <button onClick={toggleImportance}> Eliminar </button></div>
-  )
-}
-
-export default Note
