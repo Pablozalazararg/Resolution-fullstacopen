35eee64f381e58b976133b519e7d9e44023534fc
Pablozalazararg <101354957+Pablozalazararg@users.noreply.github.com>
2025-01-12 17:55:08 -0300

    Update Course.jsx

--- part2/courseinfo/Course.jsx
+++ part2/courseinfo/Course.jsx
@@ -1,24 +1,36 @@
-
-const Parts = (props) => {
-  const infoCourse = props.course.map(elemento=><li key={elemento.id}>{elemento.name} {elemento.exercises}</li>)
-  let total = 0
-  props.course.map(elemento=>total+=elemento.exercises)
-  return(
-    <>
-      <ul>
-        {infoCourse}
-      </ul>
-      <p><b>total of {total} exercises</b></p>
-    </>
+const Header = (props) => {
+  return (
+    <h1>{props.nombre}</h1>
   )
 }
-
-const Course = (props) =>{
-  
-  return(
-    <>
-      <Parts course={props.curses}></Parts>     
-    </>      
+const Part = (props) => {  
+  return (
+    <li>{props.name} {props.exercises}</li>
   )
+}
+const Content = (props) => {
+  const listItem = props.part.map(elemento=>{return <Part key={elemento.id}  name={elemento.name} exercises={elemento.exercises}></Part>}) 
+  return (
+    <div>
+      {listItem}
+    </div>
+  )
+}
+const Total = (props) => {
+  let suma=0
+  props.total.forEach(elemento=>suma+=elemento.exercises)
+  return (
+    <p>el total de ejercicios es {suma}</p>
+  )
+}
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header nombre={course.name}></Header>
+      <Content part={course.parts} ></Content>
+      <Total total={course.parts}></Total>
+    </div>
+  )
+
 }
 export default Course
