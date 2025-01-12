0c6a21499a516db3040cbcb629c155b2c9bb809d
Pablozalazararg <101354957+Pablozalazararg@users.noreply.github.com>
2025-01-12 17:54:32 -0300

    Update App.jsx

--- part2/courseinfo/App.jsx
+++ part2/courseinfo/App.jsx
@@ -1,63 +1,55 @@
-import Course from "./Course"
-const App = () => {
-  
-    const courses = [
-      {
-        name: 'Half Stack application development',
-        id: 1,
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          },
-          {
-            name: 'Redux',
-            exercises: 11,
-            id: 4
-          }
-        ]
-      }, 
-      {
-        name: 'Node.js',
-        id: 2,
-        parts: [
-          {
-            name: 'Routing',
-            exercises: 3,
-            id: 1
-          },
-          {
-            name: 'Middlewares',
-            exercises: 7,
-            id: 2
-          }
-        ]
-      }
-    ]
-  
-  const list=courses.map(course=>
-    <div key={course.id}>      
-      <h1>{course.name}</h1>
-      <Course curses={course.parts}></Course>
-    </div>
-    )
+import Course from './Course'
 
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   return (
     <>
-      {list}
-      
+      {courses.map(course=>(<Course course={course} key={course.id}></Course>))}
     </>
+    
   )
 }
 
